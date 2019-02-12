b78b902552f46eaf338006bf6d656f6f64e97011
hifromkevin <lam.kevin86@gmail.com>
2019-02-11 17:16:10 -0800

    Adds Express session middleware

--- server/app.js
+++ server/app.js
@@ -10,7 +10,22 @@ const expressValidator = require('express-validator');
 const flash = require('connect-flash');
 const session = require('express-session');
 const passport = require('passport');
-// const config = require('./config/database.js');
+const config = require('./config/database.js');
+
+// Connect to DB
+mongoose.connect(config.database, { useNewUrlParser: true });
+
+let db = mongoose.connection;
+
+// Check Connection
+db.once('open', () => {
+	console.log('DB HELLA connected');
+});
+
+// Check for DB Errors
+db.on('error', (err) => {
+	console.log(err);
+});
 
 // Load View Engines
 app.set('views', path.join(__dirname, '../views'));
@@ -19,6 +34,18 @@ app.set('view engine', 'pug');
 // Body Parser Middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+// Set Public Folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Express Session Middleware
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: true }
+})); 
+
 
 // Home Route
 app.get('/', (req, res) => {
